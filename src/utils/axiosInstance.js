7ef1bee15df1a152e96c499d88dc4eb8a65fba4a
maxwell95 <110671828+harzyz@users.noreply.github.com>
2024-07-31 02:27:36 -0700

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAccessToken, clearToken } from './auth'; 
 
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: "https://legacy-backend-zmmd.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
